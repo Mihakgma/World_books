35f7507613b0a19f9bb26e60bd022df9fca21a36
Mikhail Tabakaev <47657224+Mihakgma@users.noreply.github.com>
2022-05-18 16:55:35 +0700

    in admin module some models have been registered

--- WebBooks/catalog/admin.py
+++ WebBooks/catalog/admin.py
@@ -2,9 +2,24 @@ from django.contrib import admin
 from .models import Author, Book, Genre, Language, Status, BookInstance
 # Register your models here.
 
-admin.site.register(Author)
-admin.site.register(Book)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['last_name', 'first_name',
+                    'date_of_birth', 'date_of_death']
+
+
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
 admin.site.register(Genre)
 admin.site.register(Language)
 admin.site.register(Status)
-admin.site.register(BookInstance)
+admin.site.register(BookInstance, BookInstanceAdmin)
+
